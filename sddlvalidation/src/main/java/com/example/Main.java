e1cd8295a5708a1b30a5fb25f62fa0e2e1597b3a
Arif <33687556+arifwaqas@users.noreply.github.com>
2023-11-09 21:09:28 +0530

    Added apache.commons.codec library, changed algorithm for sd retrieval

--- sddlvalidation/src/main/java/com/example/Main.java
+++ sddlvalidation/src/main/java/com/example/Main.java
@@ -3,26 +3,25 @@ package com.example;
 import net.tirasa.adsddl.ntsd.SDDL;
 import net.tirasa.adsddl.ntsd.SID;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import com.hierynomus.msdtyp.SecurityDescriptor;
+import com.hierynomus.msdtyp.SecurityInformation;
+import com.hierynomus.smb.SMBBuffer;
+import com.hierynomus.smbj.SMBClient;
+import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.connection.Connection;
+import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.DiskShare;
+import org.apache.commons.codec.binary.Base64;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        String strjson = new String(
-                "RDpBSShBO0lEOzB4MTIwMGE5Ozs7QVUpKEE7SUQ7MHgxMjAwYTk7OztTTykoQTtJRDtGQTs7O0JBKShBO0lEO0ZBOzs7U1kp");
+    public static void validateSd(String sd) {
 
-        byte[] decoded = Base64.getDecoder().decode(strjson);
-
-        String decodedString = new String(decoded, StandardCharsets.ISO_8859_1);
-
-        System.out.println("Decoded string: " + decodedString);
-
-        String sd = new String(
-                "D:(A;ID;FA;;;SY)(A;ID;FA;;;BA)(A;ID;FA;;;S-1-5-21-2976576824-3568875664-3867986209-7232415)");
-
-        byte[] securityDescriptor = sd.getBytes(StandardCharsets.ISO_8859_1);
+        byte[] securityDescriptor = sd.getBytes();
 
         SDDL sddl = new SDDL(securityDescriptor);
 
@@ -32,11 +31,73 @@ public class Main {
 
         System.out.println(ownerSidString);
 
+    }
+
+    public static String getSd(String shareName, String folderPath) {
+
+        String hostName = new String("DCSERVER.test.local");
+        String domain = new String("test.local");
+        String userName = new String("sharad");
+        String password = new String("Attivo2$");
+
+        String sdString = new String();
+        SMBClient client = new SMBClient();
+        DiskShare share;
+        Connection connection;
+        try {
+            // making the connection ot the host
+            connection = client.connect(hostName);
+            AuthenticationContext ac = new AuthenticationContext(userName, password.toCharArray(), domain);
+
+            Session session = connection.authenticate(ac);
+
+            // Connect to Share
+            share = (DiskShare) session.connectShare(shareName);
+            /*
+             * for (FileIdBothDirectoryInformation f : share.list(folderPath)) {
+             * System.out.println("File : " + f.getFileName());
+             * }
+             */
+            // create config for retreiving the objects
+            Set<SecurityInformation> securityInformationSet = new HashSet<>();
+            securityInformationSet.add(SecurityInformation.DACL_SECURITY_INFORMATION);
+
+            // Getting the security descirptor
+            /* dirEntryPath must be SERVER.doma.in/SHARENAME/path/to/file/filename.ext */
+            String dirEntryPath = folderPath;
+            SecurityDescriptor sd = share.getSecurityInfo(dirEntryPath, securityInformationSet);
+
+            System.out.println("*******************" + sd.toString());
+
+            SMBBuffer smbBuffer = new SMBBuffer();
+            sd.write(smbBuffer);
+            sdString = new String(smbBuffer.array(), StandardCharsets.ISO_8859_1);
+
+            byte[] securityDescriptor = Base64.decodeBase64(sdString);
+
+            sdString = new String(securityDescriptor, StandardCharsets.ISO_8859_1);
+
+            System.out.println("Received security descriptor: " + sdString);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return sdString;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String sd = getSd("SYSVOL", "New Folder\\New Text Document.txt");
+        // main function
+
+        validateSd(sd);
+
         return;
 
     }
 
-    public static boolean validateSDDL(Map<String, String> mapSDDLMap) {
-        return true;
-    }
 }
