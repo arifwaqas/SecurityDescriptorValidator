de9bc03ffc4474c1236800e48e1d73ec81ed1111
arifwaqas <arif.waqas@sentinelone.com>
2023-11-16 15:30:25 +0300

    added validation of security descriptors received from external sources

--- sddlvalidation/src/main/java/com/example/Main.java
+++ sddlvalidation/src/main/java/com/example/Main.java
@@ -2,7 +2,13 @@ package com.example;
 
 import net.tirasa.adsddl.ntsd.SDDL;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -15,6 +21,7 @@ import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
 import com.hierynomus.smbj.share.DiskShare;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 
 public class Main {
 
@@ -58,12 +65,30 @@ public class Main {
 
             SMBBuffer smbBuffer = new SMBBuffer();
             sd.write(smbBuffer);
+
+            try (FileOutputStream fos = new FileOutputStream("newfile.txt")) {
+                fos.write(smbBuffer.array());
+                // fos.close(); There is no more need for this line since you had created the
+                // instance of "fos" inside the try. And this will automatically close the
+                // OutputStream
+            }
+
             sdString = new String(smbBuffer.array(), StandardCharsets.ISO_8859_1);
 
+            try (PrintWriter out = new PrintWriter("filename.txt")) {
+                out.println();
+            }
+
+            /* at RemoteSMBHandler.java */
+
             byte[] securityDescriptor = Base64.decodeBase64(sdString);
+            String decodedSdString = new String(securityDescriptor, StandardCharsets.ISO_8859_1);
 
-            SDDL sddl = new SDDL(securityDescriptor);
+            /* at VerifyGPO.java */
+            byte[] securityDescriptor2 = decodedSdString.getBytes(StandardCharsets.ISO_8859_1);
 
+            // SDDL sddl = new SDDL(securityDescriptor2);
+            SDDL sddl = new SDDL(securityDescriptor2);
             sdString = new String(securityDescriptor, StandardCharsets.ISO_8859_1);
 
             System.out.println("Received security descriptor: " + sdString);
@@ -77,10 +102,43 @@ public class Main {
 
     }
 
+
+    public static void validateSD(String filepath){
+
+        File file = new File(filepath);
+
+        try {
+            FileInputStream fin = new FileInputStream(file);
+
+            DataInputStream din = new DataInputStream(fin);
+
+            byte b[] = new byte[(int)file.length()];
+            din.read(b);
+
+            din.close();
+
+            byte[] decodedBytes = Base64.decodeBase64(b);
+            // Validation usign the library we are using at backend
+            // Our Target Function
+            SDDL sddl = new SDDL(decodedBytes);
+            // --------------
+            System.out.println("Received security descriptor: " + sddl.toString());
+
+        } catch (FileNotFoundException fe) {
+            System.out.println("FileNotFoundException : " + fe);
+        } catch (IOException ioe) {
+            System.out.println("IOException : " + ioe);
+        } catch(Exception ex){
+            System.out.println("Unknown exception caught : " + ex.getMessage());
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        String sd = getSd("SYSVOL", "New Folder\\New Text Document.txt");
+        validateSD("C:/input.txt");
+
+        
+        // String sd = getSd("SYSVOL", "New Folder\\New Text Document.txt");
         // main function
 
         return;
